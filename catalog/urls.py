aaae42b9f787c90b9832acfbd855f94087e24123
haiduk <hwlee91@gmail.com>
2020-10-15 22:59:58 -0400

    making landing page

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.urls import include
+from django.views.generic import RedirectView
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += [
+  # home page
+  path('', views.index, name='index')
+]
