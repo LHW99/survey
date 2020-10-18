5865048332f86b8a4f9ee22d586a8c416ed29a80
haiduk <hwlee91@gmail.com>
2020-10-18 02:11:53 -0400

    home page url/views

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += [
-  # home page
+urlpatterns = [
   path('', views.index, name='index')
 ]
