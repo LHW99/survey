acc33ad921cd0226c833b7f5f6c1c804a0b5046f
haiduk <hwlee91@gmail.com>
2020-10-19 02:02:00 -0400

    home page displays; start models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing URL patterns
+import uuid # required for unique instances
+from django.contrib.auth.models import User # lets us use user
+from datetime import date
 
 # Create your models here.
+class Answer(models.Model):
+  ans = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+
+class Questions(models.Model):
+  ques = models.CharField(max_length=300)
+
+  def __str__(self):
+    return self.name
+
+class Survey(models.Model):
+  def __str__(self):
+    return self.name
