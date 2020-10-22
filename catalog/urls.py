7fc369a67084c45346f64c57322f3b03c1a7c552
haiduk <hwlee91@gmail.com>
2020-10-22 03:22:35 -0400

    login and logout pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,14 +6,18 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+# home page
 urlpatterns = [
   path('', views.index, name='index')
 ]
 
+# survey display pages
 urlpatterns += [
   path('surveys/', views.SurveysListView.as_view(), name='surveys-list'),
   path('surveys/<uuid:pk>/', views.SurveysDetailView.as_view(), name='surveys-detail')
 ]
 
+# for log-ins and authentications
 urlpatterns += [
+  path('accounts/', include('django.contrib.auth.urls'))
 ]
