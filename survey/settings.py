dd22edcb0bd6dc3bcbb697df0232b4a6b62ea76e
haiduk <hwlee91@gmail.com>
2020-10-24 02:26:55 -0400

    password reset function added (makes local files instead of sending actual emails)

--- survey/settings.py
+++ survey/settings.py
@@ -123,4 +123,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# convenience that is not for production
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
