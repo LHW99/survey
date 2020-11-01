c6dc4acbae0e9cefc57cce6557ab49181541df5b
haiduk <hwlee91@gmail.com>
2020-11-01 03:05:17 -0500

    got questions inline form for survey creation, but doesn't work fully

--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,10 @@ from django.views import generic
 from catalog.models import Answers, Questions, Surveys
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+
+from django.forms.models import inlineformset_factory
+QuestionsFormset = inlineformset_factory(Surveys, Questions, fields=('question',))
 
 # Create your views here.
 
@@ -34,10 +37,30 @@ class SurveysCreate(LoginRequiredMixin, CreateView):
   model = Surveys
   fields = ['name',]
 
+  def get_context_data(self, *args, **kwargs):
+    data = super().get_context_data(**kwargs)
+    if self.request.POST:
+      data['question'] = QuestionsFormset(self.request.POST)
+    else:
+      data['question'] = QuestionsFormset()
+    return data
+  
+  def form_valid(self, form):
+    context = self.get_context_data()
+    question = context['question']
+    self.object = form.save()
+    if question.is_valid():
+      question.instance = self.object
+      question.save()
+    return super().form_valid(form)
+  
   def form_valid(self,form):
     surveyer = self.request.user
     form.instance.surveyer = surveyer
     return super(SurveysCreate, self).form_valid(form)
+
+  def get_success_url(self):
+    return reverse('surveys:list')
 
 class SurveysUpdate(LoginRequiredMixin, UpdateView):
   model = Surveys
